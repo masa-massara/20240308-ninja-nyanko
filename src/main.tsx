cb27c1a2feafaa24ae28089fd5fb1109da36c17d
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-10 03:23:24 +0900

    マニュアル作成のcss変更した

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import View_add_manual_second from './components/common/View_add_manual_second'
+import View_add_manual_third from './components/common/View_add_manual_third'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
