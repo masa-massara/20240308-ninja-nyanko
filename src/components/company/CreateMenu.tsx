92573053d15d3b1ab23ab5149e9cf5a1ba1e0593
RIKU1007223 <nango223@icloud.com>
2024-03-09 19:24:54 +0900

    マニュアル作成ページの作成

--- src/components/company/CreateMenu.tsx
+++ src/components/company/CreateMenu.tsx
@@ -1,15 +1,16 @@
 import { FC } from "react"
-
+import { Link } from "react-router-dom";
 type Props = {
     worksname: string[],
-    contents: string,
+    link: string,
     index: number,
     works: string
 }
-const CreateMenu:FC<Props> = ({worksname,index,works}) => {
+const CreateMenu:FC<Props> = ({worksname,index,works,link}) => {
   return (
     <div className="center_center">
       <div className="add_manual">
+        
         <h1>マニュアル作成({index}/3)</h1>
         <p>{works}を選択してください</p>
         <div className="divSelect">
@@ -19,11 +20,19 @@ const CreateMenu:FC<Props> = ({worksname,index,works}) => {
             })}
         </select>
         </div>
+
         <div className="textarea_input">
           <form className="form_input">
             <input type="text" className="small" placeholder="入力欄"></input>
           </form>
         </div>
+
+        <div className="button_next">
+        <button className="next">
+          <Link to={link}>次へ</Link>
+        </button>
+        </div>
+
         </div>
     </div>
   )
--- src/pages/SelectWorks.tsx
+++ src/pages/SelectWorks.tsx
@@ -1,18 +1,15 @@
 import CreateMenu from "../components/company/CreateMenu";
-import { Link } from "react-router-dom";
+
 const SelectWorks = () => {
   const industry = ["飲食店", "サービス業", "小売業", "製造業", "IT業"];
   return (
     <div>
       <CreateMenu
-        contents="店名を入力"
         index={1}
         works="業種"
         worksname={industry}
+        link="/works/position/"
       />
-      <button className="button_next">
-f        <Link to="/selectposition">次へ</Link>
-      </button>
     </div>
   );
 };
