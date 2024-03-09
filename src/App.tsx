22dea62a126f4d59080b286e3d1e7a88d62b7aca
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-03-09 13:47:13 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-
-import { useState } from 'react'
-import Modal from './components/common/modal'
 function App() {
-  // const [count, setCount] = useState(0);
+
 
 
   return (
