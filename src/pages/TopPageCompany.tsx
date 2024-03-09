932c565fdf99acf812237d96df77c56c91355cc9
RIKU1007223 <nango223@icloud.com>
2024-03-10 01:11:05 +0900

    コミットメッセージを考えたくない。

--- src/pages/TopPageCompany.tsx
+++ src/pages/TopPageCompany.tsx
@@ -2,13 +2,25 @@ import { Link } from "react-router-dom";
 
 const TopPageCompany = () => {
   return (
-    <div>
-      <h2>Arbeit Manual</h2>
-
-      <button>
+    <div className="background_gray">
+      <div className="large">
+      <h1>Arbeit Manual</h1>
+      <div className="defaultButton">
+      <button className="defaultButton_yellow">
+        <Link to="/selectworks"> マニュアルを閲覧</Link>
+      </button>
+      </div>
+      <div className="defaultButton">
+      <button className="defaultButton_red">
         <Link to="/selectworks"> マニュアルを作成</Link>
       </button>
-      <button>個人成績</button>
+      </div>
+      <div className="defaultButton">
+        <button className="defaultButton_blue">
+          <Link to="/selectworks">個人成績</Link>
+        </button>
+      </div>
+    </div>
     </div>
   );
 };
