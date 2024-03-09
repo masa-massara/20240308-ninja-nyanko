503792d61e6b0d2e36436d3f6ea719feeee6d59f
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-10 03:13:35 +0900

    tabs

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import View_manual_list from './components/common/View_manual_list'
-import View_add_manual_third from './components/common/View_add_manual_third'
-import Tabs_manual_list from './components/common/Tabs_manual_list'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <View_manual_list />
+   <App />
   </React.StrictMode>
 )
 
