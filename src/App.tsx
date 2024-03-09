be47e569bec1e6e6506502a788b59d758860b381
masa-massara <124395897+masa-massara@users.noreply.github.com>
2024-03-09 14:44:05 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
-
 function App() {
   return (
     <>
-
+      <div>
+        {/* 各ページへのルート設定 */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/addposition" element={<AddPosition />} />
+            <Route path="/selectworks" element={<SelectWorks />} />
+            <Route path="/selectposition" element={<SelectPosition />} />
+            <Route path="/toppage" element={<TopPage />} />
+            <Route path="/checkcreate" element={<CheckCreate />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </div>
 
       <div>
         <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank"></a>
       </div>
-
     </>
   );
 }
