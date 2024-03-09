5a805b72ccc39eae48a5ed3128eb57adb68e1201
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-03-10 00:08:42 +0900

    言うなれば微修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import View_manual_list from './components/common/View_manual_list'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-   <View_manual_list />
+    <App />
   </React.StrictMode>
-)
-
+);
