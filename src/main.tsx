76d800fe8437174173c2387526c034345d5e391d
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-09 20:07:12 +0900

    scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import View_manual_write from './components/common/View_manual_write'
-import View_result_quiz from './components/common/View_result_quiz'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
