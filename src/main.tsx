b0b96ec51deff8a35214a61234fe0337c5dc9231
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-09 20:06:07 +0900

    scss pullrequest

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import View_manual_write from './components/common/View_manual_write'
+import View_result_quiz from './components/common/View_result_quiz'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <App />
+  </React.StrictMode>
 )
