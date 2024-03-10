0de3e32aa6010ea15af0479543a5f2e87e5d10e0
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-11 03:25:34 +0900

    login tuini kanpeki  uso

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -9,7 +9,6 @@ import { FirebaseError } from "firebase/app";
 const Register = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -19,15 +18,12 @@ const Register = () => {
     const position = formData.get("position-select-button") as string;
     console.log(username);
     console.log(position);
-
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
-
       // ユーザープロファイルを更新
       await updateProfile(res.user, {
         displayName: username,
       });
-
       // Firestoreにユーザーデータを保存
       await setDoc(doc(db, "users", res.user.uid), {
         uid: res.user.uid,
@@ -35,18 +31,13 @@ const Register = () => {
         email,
         position,
       });
-
       const userId = res.user.uid;
-
       const docRef = doc(db, "users", userId);
       const docSnap = await getDoc(docRef);
-
       if (!docSnap.exists()) {
         return;
       }
-
       const userInfo = docSnap.data();
-
       if (userInfo.position == "company") {
         navigate("/company");
       } else {
@@ -71,45 +62,47 @@ const Register = () => {
       }
     }
   };
-
   return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo"></span>
-        <span className="title">登録</span>
-        <form onSubmit={handleSubmit}>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="email" type="email" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
-          <p>あなたの役職は？</p>
-          <input
-            title="position-select-button"
-            type="radio"
-            name="position-select-button"
-            value="company"
-            checked
-          ></input>
-          <label>社員</label>
-          <input
-            title="position-select-button"
-            type="radio"
-            name="position-select-button"
-            value="arbeit"
-          ></input>
-          <label>アルバイト</label>
-
-          <button>Sign up</button>
-          {errorMessage && <span className="error">{errorMessage}</span>}
-        </form>
-        <p>
-          Already have an account?
-          <span className="login">
-            <Link to="/login">ログイン</Link>
-          </span>
-        </p>
+    <div className="div_bg_gradient">
+      <div className="vertical-middle">
+        <div className="formContainer">
+          <div className="formWrapper">
+            <span className="logo"></span>
+            <h1 className="title">登録</h1>
+            <div className="textarea_input">
+              <form onSubmit={handleSubmit} className="form_input">
+                <input name="username" type="text" placeholder="Username" className="small" />
+                <input name="email" type="email" placeholder="Email" className="small" />
+                <input name="password" type="password" placeholder="Password" className="small" />
+                <p className="p_white">あなたの役職は？</p>
+                <input
+                  type="radio"
+                  name="position-select-button"
+                  value="company"
+                  checked
+                ></input>
+                <label>社員</label>
+                <input
+                  type="radio"
+                  name="position-select-button"
+                  value="arbeit"
+                ></input>
+                <label>アルバイト</label>
+                <button className="button_login">新規登録をする</button>
+                {errorMessage && <span className="error">{errorMessage}</span>}
+              </form>
+            </div>
+            <p className="p_white">
+              Already have an account?&ensp;
+              <span className="login">
+                <Link to="/login" className="link_decoration">ログイン</Link>
+              </span>
+            </p>
+            {/* <Voice></Voice> */}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
-
 export default Register;
