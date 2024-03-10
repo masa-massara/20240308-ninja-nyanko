295cb3cc1c17df1fc4b6938b8e9d2a67ce7cee3d
RIKU1007223 <nango223@icloud.com>
2024-03-10 10:48:35 +0900

    インテンドの修正

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import TopPageArbeit from "./pages/TopPageArbeit";
 import TopPageCompany from "./pages/TopPageCompany";
-import CreateQuiz from "./pages/SelectQuiz";
 import ResultList from "./pages/ResultList";
 import ManualListCompany from "./pages/ManualListCompany";
 import Quizmenu from "./pages/Quizmenu";
@@ -21,11 +20,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
 
-
-
             <Route path="/works" element={<SelectWorks />} />
             <Route path="/works/position" element={<SelectPosition />} />
-            <Route path="/works/position/addposition" element={<AddPosition position="レジ"/>} />
+            <Route
+              path="/works/position/addposition"
+              element={<AddPosition position="レジ" />}
+            />
             <Route path="/checkcreate" element={<CheckCreate />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
