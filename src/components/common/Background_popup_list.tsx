46462fa1d83e7a575459760046c129add2ef0e62
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-10 16:40:11 +0900

    owata

---
+++ src/components/common/Background_popup_list.tsx
@@ -0,0 +1,15 @@
+import Button_small_black from "./Button_small_black"
+
+const Background_popup_list = () => {
+  return (
+    <div className="background_popup_manual">
+
+      <p className="pManualListComponent">text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text texttext text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text  text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+      <div className="imgManualListComponent"></div>
+
+      <Button_small_black />
+    </div>
+  )
+}
+
+export default Background_popup_list
---
+++ src/components/common/Button_small_black.tsx
@@ -0,0 +1,9 @@
+const Button_small_black = () => {
+  return (
+    <div className="button_small_color">
+      <button className="black">閉じる</button>
+    </div>
+  )
+}
+
+export default Button_small_black
