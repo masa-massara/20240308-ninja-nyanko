49b14e1dc009c91ce6c095c7f9e555e418a53316
yukiringo <141327996+yukiringo-apple@users.noreply.github.com>
2024-03-10 12:19:40 +0900

    登録画面終わったーチェックボックス後回しします:)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -50,7 +50,7 @@ const Login = () => {
           <div className="formWrapper">
             <h1 className="title">ログイン</h1>
 
-           
+
             <div className="textarea_input">
               <form onSubmit={handleSubmit} className="form_input">
                 <input type="email" placeholder="Email" name="email" className="small" />
@@ -59,7 +59,7 @@ const Login = () => {
                 {err && <span className="error_message">エラーが発生しました</span>}
               </form>
             </div>
-            <p className="p_white">アカウントを持っていませんか？ <Link to={"/register"} className="link_decoration">登録</Link></p>
+            <p className="p_white">アカウントを持っていませんか？&ensp;<Link to={"/register"} className="link_decoration">登録</Link></p>
           </div>
         </div>
       </div>
