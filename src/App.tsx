6f851fd08f9ecc79282ce2f8077d54a72d06e9c4
RIKU1007223 <nango223@icloud.com>
2024-03-10 17:48:05 +0900

    インポートの修正

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import TopPageArbeit from "./pages/TopPageArbeit";
 import TopPageCompany from "./pages/TopPageCompany";
 import ResultList from "./pages/ResultList";
 import ManualListCompany from "./pages/ManualListCompany";
-import CreateQuiz from "./components/staff/CreateQuiz";
+import Quizmenu from "./pages/Quizmenu";
 
 
 type PlaceContextType = {
@@ -64,7 +64,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/arbeit" element={<TopPageArbeit />} />
-              <Route path="/arbeit/selectquiz" element={<CreateQuiz />} />
+              <Route path="/arbeit/selectquiz" element={<Quizmenu />} />
               <Route path="/company" element={<TopPageCompany />} />
               <Route path="/company/resultlist" element={<ResultList />} />
               <Route path="/company/manual" element={<ManualListCompany />} />
--- src/components/company/CheckModal.tsx
+++ src/components/company/CheckModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 // ]]import { useNavigate } from "react-router-dom";
 type Props = {
   contents: string;
