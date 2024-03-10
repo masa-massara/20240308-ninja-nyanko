0e31b150a91492dcdc6e47edeb052471639aa4e8
RIKU1007223 <nango223@icloud.com>
2024-03-10 11:11:59 +0900

    リンクの追加

--- src/pages/Quizmenu.tsx
+++ src/pages/Quizmenu.tsx
@@ -1,23 +1,31 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import CreateQuiz from "../components/staff/CreateQuiz";
 const Quizmenu = () => {
-      const stage = [
-        "店長",
-        "ホールスタッフ",
-        "キッチンスタッフ",
-        "サービススタッフ",
-        "その他",
-      ];
-      return(
-      <div className='center_center'>
-        <div className='add_manual'>
-          <CreateQuiz stage={stage} />
-          <div className="defaultButton" >
-            <button className="defaultButton_yellow">クイズスタート</button>
-          </div>
+  const stage = [
+    "店長",
+    "ホールスタッフ",
+    "キッチンスタッフ",
+    "サービススタッフ",
+    "その他",
+  ];
+  return (
+    <div className="center_center">
+      <div className="add_manual">
+        <CreateQuiz stage={stage} />
+        <div className="defaultButton">
+          <button className="defaultButton_yellow">
+            <Link to="/arbeit/quiz">クイズスタート</Link>
+          </button>
         </div>
+        <p>
+          マニュアルを見たい人は<Link to="/ManualListPart">こちら</Link>
+        </p>
+        <p>
+          個人成績は<Link to="/company/resultlist">こちら</Link>
+        </p>
       </div>
-      );
+    </div>
+  );
 };
 
 export default Quizmenu;
