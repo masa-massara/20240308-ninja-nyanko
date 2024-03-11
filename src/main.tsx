9f909b6b592fbbe1e2332e5d13e53439f772ca4e
hiromu <131576261+hiromuota166@users.noreply.github.com>
2024-03-11 10:18:29 +0900

    Update main.tsx
    戻しました

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./scss/style.scss";
 import App from "./App";
-import View_add_manual_second from "./components/common/View_add_manual_second";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    
-       <App />
-
-   
-      </React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
